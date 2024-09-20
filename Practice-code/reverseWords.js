5ed0a3e3488444983dac573387f57c4beb177afe
karanShinde7 <karan.shinde27@gmail.com>
2024-09-20 08:56:14 +0530

    Reverser words

---
+++ Practice-code/reverseWords.js
@@ -0,0 +1,7 @@
+const reverseOrder = (str) => {
+    console.log("Given string: ", str);
+
+    console.log(str.split(' ').reverse().join(' '));
+}
+
+reverseOrder("JavaScript to reverse the order of words ");
